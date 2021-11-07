b26f2de9dd92b40fd746b15ba7d55bab86f39412
Alejandro Álvarez <alealvpav@gmail.com>
2021-11-07 13:53:03 +0100

    Create model Idea

--- ideas/models.py
+++ ideas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from profiles.models import Profile
+
+
+class Idea(models.Model):
+
+    # Visibility options
+    PUBLIC = "PUB"  # Public: Everyone can see the idea
+    PROTECTED = "PRO"  # Only accepted followers can see the idea
+    PRIVATE = "PRI"  # Only the owner can see the idea.
+    VISIBILITY_OPTIONS = [
+        (PUBLIC, "Public"),
+        (PROTECTED, "Protected"),
+        (PRIVATE, "Private"),
+    ]
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500, blank=False, null=False)
+    visibility = models.CharField(
+        max_length=3, choices=VISIBILITY_OPTIONS, default=PUBLIC
+    )
+    created = models.DateTimeField(auto_now_add=True)
