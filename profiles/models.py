c05f988074914bf9d9196fdab086f244910b7d73
Alejandro Álvarez <alealvpav@gmail.com>
2021-11-07 19:57:38 +0100

    Add followers and FollowRequest

--- profiles/models.py
+++ profiles/models.py
@@ -13,9 +13,41 @@ class User(AbstractUser):
 class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    followers = models.ManyToManyField("self", symmetrical=False, blank=True)
 
     def __str__(self) -> str:
         return str(self.user)
+
+
+class FollowRequest(models.Model):
+
+    # Statusses
+    PENDING = "PEN"
+    APPROVED = "APP"
+    REJECTED = "REJ"
+    STATUSES = [(PENDING, "Pending"), (APPROVED, "Approved"), (REJECTED, "Rejected")]
+
+    requestor = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="requests",
+        related_query_name="request",
+    )
+    requested = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="pending_requests",
+        related_query_name="pending_request",
+    )
+    status = models.CharField(max_length=3, choices=STATUSES, default=PENDING)
+
+    def approve(self):
+        self.status = self.APPROVED
+        self.save()
+
+    def reject(self):
+        self.status = self.REJECTED
+        self.save()
 
 
 @receiver(post_save, sender=User)
