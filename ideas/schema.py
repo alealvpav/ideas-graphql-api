5e4cbe2ab2e6d1586f7e9701092874b9c43181d5
Alejandro Álvarez <alealvpav@gmail.com>
2021-11-09 18:03:32 +0100

    Add timeline ideas query

--- ideas/schema.py
+++ ideas/schema.py
@@ -13,6 +13,7 @@ class Query(graphene.ObjectType):
     ideas = graphene.List(IdeaType)
     public_ideas = graphene.List(IdeaType)
     my_ideas = graphene.List(IdeaType)
+    timeline = graphene.List(IdeaType)
 
     def resolve_ideas(self, info, **kwargs):
         """
@@ -38,6 +39,21 @@ class Query(graphene.ObjectType):
         return user.profile.get_my_ideas()
         return user.profile.idea_set.all()
 
+    def resolve_timeline(self, info, **kwargs):
+        """
+        List the timeline (Ideas) of a logged User. This means all the ideas of the
+        logged user and the PUBLIC and PROTECTED of the Profiles he follows (ordered by descending date).
+        - Un usuario puede ver un timeline de ideas compuesto por sus propias ideas y las ideas de los usuarios a los que sigue, teniendo en cuenta la visibilidad de cada idea.
+        """
+        user = info.context.user
+        check_user_logged(user)
+        # Own ideas
+        own_ideas = user.profile.get_my_ideas()
+        # Ideas of the profiles followed by the user
+        followed_ideas = Idea.objects.exclude(visibility=Idea.PRIVATE).filter(
+            profile__followers=user.profile
+        )
+        return (own_ideas | followed_ideas).distinct().order_by("-created")
 
 class CreateIdea(graphene.Mutation):
     id = graphene.Int()
